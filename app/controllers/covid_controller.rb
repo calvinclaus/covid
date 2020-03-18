a4fc174855691396958d8aea42e3e2d4f36948a2
Calvin Claus <calvinclaus@me.com>
2020-03-18 08:45:27 +0100

    Rohdaten Download

--- app/controllers/covid_controller.rb
+++ app/controllers/covid_controller.rb
@@ -9,4 +9,26 @@ class CovidController < ApplicationController
     @statistics_json = render_to_string(template: 'covid/show.json.jbuilder', formats: 'json', layout: false)
     render :show, formats: [:html]
   end
+
+  def raw_data
+      header = [ "date", "num_tested", "num_infected", "num_recovered", "num_dead", ]
+
+      statistics = Statistic.order(at: :asc)
+
+      rows = statistics.map do |s|
+        [
+          s.at.to_s,
+          s.num_tested,
+          s.num_infected,
+          s.num_recovered,
+          s.num_dead,
+        ]
+      end
+
+      csv_string = [header].concat(rows).inject([]){ |csv, row| csv << CSV.generate_line(row) }.join("")
+      file = Tempfile.new("rohdaten-cov19-at")
+      file.write(csv_string)
+      file.close
+      send_file file.path, filename: "rohdaten-cov19-at.csv", type: "text/csv; charset=utf-8"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/', to: 'covid#show', as: :show_covid
+  get 'raw_data', to: 'covid#raw_data', as: :raw_data
 end

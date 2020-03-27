6ebefc5835c59197991b6cef057e95600e0a1b4a
Calvin Claus <calvinclaus@me.com>
2020-03-27 18:38:02 +0100

    updated crawling code to new structure of sozialministerium content

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -3,7 +3,7 @@ class Statistic < ApplicationRecord
   USER_AGENT = 'Opera/9.63 (Macintosh; Intel Mac OS X; U; en) Presto/2.1.1'.freeze
   def self.maybe_load_newest
     agent = request("https://www.sozialministerium.at/Informationen-zum-Coronavirus/Neuartiges-Coronavirus-(2019-nCov).html")
-    text = agent.css(".abstract").first.text
+    text = agent.css("#content").first.text
 
     begin
       maybe_set_newest_from_text(text)
@@ -17,21 +17,18 @@ class Statistic < ApplicationRecord
     hash = { }
 
     text.split("\n").each do |line|
-      if line.downcase.include?("uhr") || line.downcase.include?("stand")
+      if line.downcase.include?("stand") && line.downcase.include?("bestätigt") && hash[:at].blank?
         hash[:at] = DateTime.parse(line).asctime.in_time_zone("Europe/Vienna")
       end
 
-      number_line = line.gsub(".", "").downcase
-      if number_line.include? "test"
+      number_line = line.downcase.gsub(/\d\d\:\d\d uhr/, "").gsub(/\d\d\.\d\d.\d\d\d\d/, "").gsub(".", "")
+      if hash[:num_tested].blank? && number_line.include?("test")
         hash[:num_tested] = number_line.scan(/\d+/).first
       end
-      if number_line.include?(" fälle") || number_line.include?("bestätigt")
+      if hash[:num_infected].blank? && (number_line.include?(" fälle") || number_line.include?("bestätigt"))
         hash[:num_infected] = number_line.scan(/\d+/).first
       end
-      if number_line.include?("genesen") || number_line.include?("gesund")
-        hash[:num_recovered] = number_line.scan(/\d+/).first
-      end
-      if number_line.include?("tod") || number_line.include?("tot")
+      if hash[:num_dead].blank? && (number_line.include?("tod") || number_line.include?("tot"))
         hash[:num_dead] = number_line.scan(/\d+/).first
       end
     end
